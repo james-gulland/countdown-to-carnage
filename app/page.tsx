3a731be333ef5b13ec772dce09ac2f91e738bb35
James Gulland <jamesgulland@gmail.com>
2025-07-29 21:26:03 +0100

    add background image

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
+import Image from "next/image";
 import { Countdown } from "./components/Countdown";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gray-900 flex flex-col items-center justify-center">
-      <h1 className="text-white">Hello</h1>
+    <main className="relative min-h-screen w-full flex flex-col items-center justify-center overflow-hidden">
+      <Image
+        src="/images/fold.jpeg"
+        alt="Background"
+        fill
+        priority
+        className="object-cover z-[-1]"
+      />
+
+      {/* Overlay content */}
+      <h1 className="text-white text-4xl mb-4">Countdown to Carnage</h1>
       <Countdown />
     </main>
   );
