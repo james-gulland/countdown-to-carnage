5d86b4ec61f63777bb52963b3aa8bb8f76c200e9
James Gulland <jamesgulland@gmail.com>
2025-07-29 21:11:30 +0100

    add countdown component

---
+++ app/components/Countdown.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React from "react";
+import FlipClockCountdown from "@leenguyen/react-flip-clock-countdown";
+import '@leenguyen/react-flip-clock-countdown/dist/index.css';
+
+
+// Target date (can update this anytime)
+const targetDate = new Date("2025-09-26T22:44:59").getTime();
+
+export const Countdown = () => {
+  return (
+    <div className="flex justify-center items-center bg-black">
+      <FlipClockCountdown
+        to={targetDate}
+        labels={["DAYS", "HOURS", "MINUTES", "SECONDS"]}
+        labelStyle={{
+          fontSize: 12,
+          letterSpacing: 2,
+          color: "#888",
+          marginTop: 8,
+        }}
+        digitBlockStyle={{
+          width: 60,
+          height: 80,
+          fontSize: 40,
+          background: "#111",
+          color: "#fff",
+        }}
+        duration={0.6}
+      />
+    </div>
+  );
+};
